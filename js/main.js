ce998ec65bcd292d9d4ce73a4e3ae198c4180866
Narayan Pradhan <narayan.pradhan1117@gmail.com>
2023-08-09 20:57:13 +0530

    corrected validation issues

--- js/main.js
+++ js/main.js
@@ -53,20 +53,24 @@ function topFunction() {
   document.documentElement.scrollTop = 0; 
 }
 
-function sendEmail(){
-  let email = document.getElementById("email").value;
-  let message = document.getElementById("message").value;
-  let name = document.getElementById("name").value;
-  let body = "Name: "+name+"<br/>Message: "+message+"<br/>Email: "+email;
-  Email.send({
-    SecureToken : "3b5661d4-c662-490b-95ab-ed4085e6ec0f",
-    To : 'narayanpradhanb1117@gmail.com',
-    From : "narayanpradhanb1117@gmail.com",
-    Subject : "PORTFOLIO FORM COMMUNICATION",
-    Body : body
-  }).then(
-  message => alert(message)
-  );
+function sendEmail() {
+  // Only send the email if the validation succeeded
+  if (validate()) {
+      let email = document.getElementById("email").value;
+      let message = document.getElementById("message").value;
+      let name = document.getElementById("name").value;
+      let body = "Name: " + name + "<br/>Message: " + message + "<br/>Email: " + email;
+
+      Email.send({
+          SecureToken: "3b5661d4-c662-490b-95ab-ed4085e6ec0f",
+          To: 'narayanpradhanb1117@gmail.com',
+          From: "narayanpradhanb1117@gmail.com",
+          Subject: "PORTFOLIO FORM COMMUNICATION",
+          Body: body
+      }).then(
+          message => alert(message)
+      );
+  }
 }
 
 // 3b5661d4-c662-490b-95ab-ed4085e6ec0f
