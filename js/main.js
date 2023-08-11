3f4b61577548136f5fa68af3ca597b0046e69775
Narayan Pradhan <narayan.pradhan1117@gmail.com>
2023-08-11 11:52:21 +0530

    fixed email bug

--- js/main.js
+++ js/main.js
@@ -1,38 +1,40 @@
 function validate(e) {
-    let name = document.getElementById("name").value
-    let email = document.getElementById("email").value
+  let name = document.getElementById("name").value;
+  let email = document.getElementById("email").value;
 
+  let name_error = document.getElementById("name_error");
+  let email_error = document.getElementById("email_error");
 
-    let name_error = document.getElementById("name_error")
-    let email_error = document.getElementById("email_error")
- 
+  let error = false;
 
-    let error = false
-    if (name==""){
-        name_error.innerHTML = "*Name is required";
-        error = true;
+  if (name === "") {
+      name_error.innerHTML = "*Name is required";
+      error = true;
+  } else {
+      name_error.innerHTML = "";
+  }
+  
+  let email_pattern = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/;
+  
+  if (email === "") {
+      email_error.innerHTML = "Email is a required field";
+      error = true;
+  } else if (!email.match(email_pattern)) {
+      email_error.innerHTML = "*Please enter a valid email id";
+      error = true;
+  } else {
+      email_error.innerHTML = "";
+  }
+
+  if (error) {
+      console.log("there is error");
+      e.preventDefault();
+      document.getElementById("form").classList.add("was-validated");
     }
-    else{
-        name_error.innerHTML=""
-    }
-    
-    let email_pattern = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/
-    if(email==""){
-        email_error.innerHTML = "email is a required field";
-        error = true;
-    }
-    else if(!email.match(email_pattern)){
-        email_error.innerHTML="*Please enter a valid email id";
-        error = true;
-    }
-    else{
-        email_error.innerHTML = ""
-    }
-       
-    if(error){
-        e.preventDefault()
-        document.getElementById("form").classList.add("was-validated")
-    }
+  if (!error) {
+    console.log("there is no error");
+    sendEmail();
+}
 
 }
 
@@ -53,24 +55,21 @@ function topFunction() {
   document.documentElement.scrollTop = 0; 
 }
 
-function sendEmail() {
-  // Only send the email if the validation succeeded
-  if (validate()) {
-      let email = document.getElementById("email").value;
-      let message = document.getElementById("message").value;
-      let name = document.getElementById("name").value;
-      let body = "Name: " + name + "<br/>Message: " + message + "<br/>Email: " + email;
-
-      Email.send({
-          SecureToken: "3b5661d4-c662-490b-95ab-ed4085e6ec0f",
-          To: 'narayanpradhanb1117@gmail.com',
-          From: "narayanpradhanb1117@gmail.com",
-          Subject: "PORTFOLIO FORM COMMUNICATION",
-          Body: body
+function sendEmail(){
+  console.log("send email is called");
+  let email = document.getElementById("email").value;
+  let message = document.getElementById("message").value;
+  let name = document.getElementById("name").value;
+  let body = "Name: " + name + "<br/>Message: " + message + "<br/>Email: " +email;
+  Email.send({
+        SecureToken: "3b5661d4-c662-490b-95ab-ed4085e6ec0f",
+        To: 'narayanpradhanb1117@gmail.com',
+        From: "narayanpradhanb1117@gmail.com",
+        Subject: "PORTFOLIO FORM COMMUNICATION",
+        Body: body
       }).then(
-          message => alert(message)
+          message => alert("email successful")
       );
-  }
 }
 
 // 3b5661d4-c662-490b-95ab-ed4085e6ec0f
